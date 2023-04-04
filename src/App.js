a972f283318056680b6e4811c30f3222e97c9a6e
nithishkk <109412506+nithishkk@users.noreply.github.com>
2023-04-04 23:52:54 +0530

    1 file modified updated

--- src/App.js
+++ src/App.js
@@ -54,7 +54,7 @@ export default function App() {
      
       
     
-      {name && <p>Start editing to see some magic happen :)</p>}
+      {love&& <p>i love you forver :)</p>}
   
     </div>
   );
