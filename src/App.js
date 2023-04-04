fc6e27869e3e267fcae59312257b61e893022858
nithishkk <109412506+nithishkk@users.noreply.github.com>
2023-04-04 23:50:57 +0530

    updated baseds on ID

--- src/App.js
+++ src/App.js
@@ -1,30 +1,64 @@
 import React,{useState} from "react";
 import "./style.css";
-const data=['nithish']
+const d=[
+  { id:1,
+    name:"nithish"
+
+  },
+  {
+    id:2,
+    name:"Meghana"
+
+  },
+{
+  id:3,
+  name:"sonu"
+
+}
+]
+  
 export default function App() {
+  const[love,setLove]=useState('')
+  const[nithish,setNithish]=useState('')
  const[name,setName]=useState(false)
   const[data,setData]=useState(99)
-  function Submit(e){
-    if(e.currentTarget.id){
-      setData(e.currentTarget.id)
+  function Submit(e,n){
+    console.log(n,"gg")
+  if(n==="love")
+      setLove(n)
+
+    
+    if(e.target.value){
+      setData(e.currentTarget.value)
       // setName(false)
-       setName(e.currentTarget.id)
+       setName(e.currentTarget.value)
     } else {
       setName(false)
     }
    
-    console.log(e.currentTarget.id,"id")
+    console.log(e.target.value,"id")
 
   }
   return (
     <div>
+      
       {data}
       <h1>Hello StackBlitz!</h1>
-      <button id='true' onClick={(e)=>Submit(e)}
+      {love}
+     {d.map((item,index)=>{
+    return(
+      <button id='true' value={nithish} onClick={(e)=>Submit(e,item.name==='sonu' ?'love':"lover")} > update</button>
+    )
       
-      > update</button>
+     })}
+     
+      
+    
       {name && <p>Start editing to see some magic happen :)</p>}
   
     </div>
   );
 }
+// onChange={(e) => {
+//   onChangeRequestAmount(e, owner_id);
+// }}
